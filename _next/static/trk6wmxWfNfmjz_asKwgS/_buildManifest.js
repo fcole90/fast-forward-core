b15c64b384d1421ccb37b8c449e78316b923f019
fcole90 <fcole90@users.noreply.github.com>
2023-01-28 00:47:41 +0000

    deploy: 2870f107d4047291eeb08652c324b461dbab132f

---
+++ _next/static/trk6wmxWfNfmjz_asKwgS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/128-2eef260e02f54a90.js","static/chunks/pages/index-0c66c564e29cac29.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
